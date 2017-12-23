63a7f463515b0f577b995c821e42c1cd23926be0
xc <351135612@qq.com>
2017-12-23 12:33:28 +0800

    init app

---
+++ app/src/main/java/cn/itgrocery/pocketc/ui/activity/main/MainActivity.java
@@ -0,0 +1,17 @@
+package cn.itgrocery.pocketc.ui.activity.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import cn.itgrocery.pocketc.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
