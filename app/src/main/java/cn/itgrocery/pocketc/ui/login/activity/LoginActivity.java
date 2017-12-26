d5babac9e46390775c9970c81a1b8785e9867a1f
chenxl <shannonc@163.com>
2017-12-26 17:17:48 +0800

    开始编写倒计时功能

--- app/src/main/java/cn/itgrocery/pocketc/ui/login/activity/LoginActivity.java
+++ app/src/main/java/cn/itgrocery/pocketc/ui/login/activity/LoginActivity.java
@@ -1,14 +1,63 @@
 package cn.itgrocery.pocketc.ui.login.activity;
 
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import butterknife.BindView;
+import butterknife.BindViews;
 import cn.itgrocery.pocketc.R;
 import cn.itgrocery.pocketc.base.SimpleActivity;
+import cn.itgrocery.pocketc.util.SnackbarUtil;
 
 public class LoginActivity extends SimpleActivity {
 
+    @BindView(R.id.input_phone_number)
+    private MaterialEditText inputPhoneNumber;
+    @BindView(R.id.input_identitfy_code)
+    private MaterialEditText inputIdentitfyCode;
+    @BindView(R.id.get_identitfy_code)
+    private EditText getIdentitfyCode;
+
+    @BindView(R.id.btn_login)
+    private Button btnLogin;
+
+    //倒计时
+    private CountDownTimer countDownTimer;
+    private boolean isCountDown = false;
 
     @Override
     protected void initEventAndData() {
+        countDownTimer = new CountDownTimer(60000,1000) {
+            @Override
+            public void onTick(long l) {
 
+            }
+
+            @Override
+            public void onFinish() {
+                isCountDown = false;
+            }
+        };
+
+        getIdentitfyCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String phoneNumber = inputPhoneNumber.getText().toString();
+                String identitfyCode = inputIdentitfyCode.getText().toString();
+
+            }
+        });
     }
 
     @Override
